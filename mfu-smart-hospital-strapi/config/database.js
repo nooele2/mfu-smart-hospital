7fad5b51bff321a2ea73ce3605de65a088457d28
nooele2 <noellehtoo@gmail.com>
2025-11-20 13:37:24 +0700

    Configure Strapi for production deployment with PostgreSQL

---
+++ mfu-smart-hospital-strapi/config/database.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      connectionString: env('DATABASE_URL'),
+      ssl: env.bool('DATABASE_SSL', false) ? {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+      } : false,
+    },
+    debug: false,
+  },
+});
--- mfu-smart-hospital-strapi/config/middlewares.ts
+++ mfu-smart-hospital-strapi/config/middlewares.ts
@@ -10,3 +10,38 @@ export default [
   'strapi::favicon',
   'strapi::public',
 ];
+
+module.exports = [
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['*'], // We'll restrict this later with your Vercel URL
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
