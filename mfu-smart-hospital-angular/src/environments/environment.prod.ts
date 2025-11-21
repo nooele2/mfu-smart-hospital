9022c1071f734277a90c53ee4fa2aec508f0b187
nooele2 <noellehtoo@gmail.com>
2025-11-21 16:25:23 +0700

    update

---
+++ mfu-smart-hospital-angular/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://mfu-smart-hospital.onrender.com',
+  strapiUrl: 'https://mfu-smart-hospital.onrender.com'
+};
---
+++ mfu-smart-hospital-angular/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:1337',
+  strapiUrl: 'http://localhost:1337'
+};
